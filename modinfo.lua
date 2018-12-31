1ddfe238bef871c5ed28a661965d9be169c5db3b
Alberto Romañach <albertoromanach@gmail.com>
2018-12-31 01:37:38 -0500

    Chest copied and added/removed from QUANTA
    Chest will add/remove itself from TUNING.QUANTA upon creation/destruction or burning.

---
+++ modinfo.lua
@@ -0,0 +1,18 @@
+name = "Quantum Chest"
+description = "More than one chest, but one storage space. Access the same item from multiple chests"
+author = "rawii22 & lord_of_les_ralph"
+version = "1.0"
+
+forumthread = ""
+
+api_version = 10
+
+priority = - 1
+dst_compatible = true
+all_clients_require_mod = true
+client_only_mod = false
+
+configuration_options =
+{
+
+}
---
+++ modmain.lua
@@ -0,0 +1,84 @@
+PrefabFiles = 
+{
+	"treasurechest_quantum",
+}
+
+Assets = 
+{
+	Asset( "IMAGE", "minimap/treasurechest_quantum.tex" ),
+	Asset( "ATLAS", "minimap/treasurechest_quantum.xml" ),
+	Asset( "IMAGE", "images/inventoryimages/treasurechest_quantum.tex" ),
+	Asset( "ATLAS", "images/inventoryimages/treasurechest_quantum.xml" ),
+}
+AddMinimapAtlas("minimap/treasurechest_quantum.xml")
+
+GLOBAL.STRINGS.NAMES.TREASURECHEST_QUANTUM = "Quantumly Entangled Chest"
+GLOBAL.STRINGS.RECIPE_DESC.TREASURECHEST_QUANTUM = "Instant accessibility."
+GLOBAL.STRINGS.CHARACTERS.GENERIC.DESCRIBE.TREASURECHEST_QUANTUM = "This could be useful..."
+GLOBAL.STRINGS.CHARACTERS.WICKERBOTTOM.DESCRIBE.TREASURECHEST_QUANTUM = "Quantum mechanics is not my specialty, but I won't question it."
+GLOBAL.STRINGS.CHARACTERS.WENDY.DESCRIBE.TREASURECHEST_QUANTUM = "Can it teleport my heart away?"
+GLOBAL.STRINGS.CHARACTERS.WAXWELL.DESCRIBE.TREASURECHEST_QUANTUM = "Oh Charlie, what have you done..."
+
+AddRecipe("treasurechest_quantum", {GLOBAL.Ingredient("boards", 3), GLOBAL.Ingredient("nightmarefuel", 2)}, GLOBAL.RECIPETABS.MAGIC, GLOBAL.TECH.MAGIC_ONE, "treasurechest_placer", 1, nil, 1, nil, "images/inventoryimages/treasurechest_quantum.xml", "treasurechest_quantum.tex")
+
+GLOBAL.TUNING.QUANTA = {}
+
+GLOBAL.unentangle = function(inst)
+	print(tostring(inst).." has been unentangled(aka removed to TUNING.QUANTA).")
+	local position = nil
+	for k, v in pairs(TUNING.QUANTA) do
+		if v.GUID == inst.GUID then
+			position = k
+		end
+	end
+	table.remove(TUNING.QUANTA, position)
+end
+
+local function quantumburnt(component)
+	if component.inst:HasTag("quantum") then
+		component.inst:ListenForEvent("onburnt", GLOBAL.unentangle)
+	end
+end
+	
+AddComponentPostInit("burnable", quantumburnt)
+
+--------------------------------------------------------------------------
+--[[ treasurechest_quantum ]]
+--------------------------------------------------------------------------
+--since 'params' is localin 'containers.lua'
+local params = {}
+
+params.treasurechest_quantum =
+{
+    widget =
+    {
+        slotpos = {},
+        animbank = "ui_chest_3x3",
+        animbuild = "ui_chest_3x3",
+        pos = GLOBAL.Vector3(0, 200, 0),
+        side_align_tip = 160,
+    },
+    type = "chest",
+}
+
+for y = 2, 0, -1 do
+    for x = 0, 2 do
+        table.insert(params.treasurechest_quantum.widget.slotpos, GLOBAL.Vector3(80 * x - 80 * 2 + 80, 80 * y - 80 * 2 + 80, 0))
+    end
+end
+
+local containers = GLOBAL.require "containers"
+containers.MAXITEMSLOTS = math.max(containers.MAXITEMSLOTS, params.treasurechest_quantum.widget.slotpos ~= nil and #params.treasurechest_quantum.widget.slotpos or 0)
+local old_widgetsetup = containers.widgetsetup
+function containers.widgetsetup(container, prefab, data)
+	local pref = prefab or container.inst.prefab
+	if pref == "treasurechest_quantum" then
+		local t = params[pref]
+		for k, v in pairs(t) do
+			container[k] = v
+		end
+		container:SetNumSlots(container.widget.slotpos ~= nil and #container.widget.slotpos or 0)
+	else
+		return old_widgetsetup(container, prefab)
+	end
+end
